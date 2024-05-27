847e5197dc893786761065967f2aea52a9696e0f
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-05-27 09:47:16 +0300

    chore:have you installed tailwindcss

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c5db95924688a736"; }
+/******/ 		__webpack_require__.h = function() { return "5ae99e0c33a32d78"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
