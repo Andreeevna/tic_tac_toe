0f352430e756b0fa4e00fdad75430aad023c9b84
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-05-27 11:46:28 +0300

    feat: added tailwindcss

--- components/game/game.jsx
+++ components/game/game.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import useGameState from './use-game-state'
 
-import styles from './Game.module.css'
 import { GameCell } from './game-cell'
 import { GameInfo } from './game-info'
+import { ResetButton } from './reset-button'
 
 export const Game = () => {
 	const {
@@ -16,7 +16,7 @@ export const Game = () => {
 		isDraw,
 	} = useGameState()
 	return (
-		<div className={styles.game}>
+		<div className='flex flex-col items-center w-40 mx-auto my-24 border border-black p-5'>
 			<GameInfo
 				isDraw={isDraw}
 				winnerSeq={winnerSeq}
@@ -24,7 +24,7 @@ export const Game = () => {
 				currentStep={currentStep}
 			/>
 
-			<div className={styles.game_field}>
+			<div className='grid pt-px pl-px grid-cols-[repeat(3,_30px)] grid-rows-[repeat(3,_30px)]'>
 				{cells.map((symbol, index) => {
 					return (
 						<GameCell
@@ -36,9 +36,8 @@ export const Game = () => {
 					)
 				})}
 			</div>
-			<button className={styles.reset} onClick={handleResetClick}>
-				Сбросить
-			</button>
+
+			<ResetButton onClick={handleResetClick} />
 		</div>
 	)
 }
---
+++ components/game/reset-button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ResetButton = ({ onClick }) => {
+	return (
+		<button
+			className='mt-2 py-1 px-3 bg-transparent rounded-sm border border-gray-400 cursor-pointer'
+			onClick={onClick}
+		>
+			Сбросить
+		</button>
+	)
+}
