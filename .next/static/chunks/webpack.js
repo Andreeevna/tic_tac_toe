19b952558401aec3715592744dbe74e35ba58b43
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-06-20 12:05:21 +0300

    refactor: we have made a new display of characters in the field

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f5e5f95da860b94"; }
+/******/ 		__webpack_require__.h = function() { return "877963237485a53b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/game-new/game.jsx
+++ components/game-new/game.jsx
@@ -1,11 +1,23 @@
+import { useGameState } from './model/use-game-state'
 import { BackLink } from './ui/back-link'
 import { players } from './ui/constants'
+import { GameCell } from './ui/game-cell'
 import { Gameinfo } from './ui/game-info'
 import { GameLayout } from './ui/game-layout'
+import { GameMoveInfo } from './ui/game-move-info'
 import { Gametitle } from './ui/game-title'
 import { PlayerInfo } from './ui/player-info'
 
+const PLAYERS_COUNT = 4
 export const Game = () => {
+	const {
+		cells,
+		currentMove,
+		nextMove,
+		handleCellClick,
+		winnerSequence,
+		winnerSymbol,
+	} = useGameState(PLAYERS_COUNT)
 	return (
 		<GameLayout
 			backLink={<BackLink />}
@@ -22,6 +34,18 @@ export const Game = () => {
 					seconds={60}
 					symbol={player.symbol}
 					isRight={index % 2 === 1}
+				/>
+			))}
+			gameMoveInfo={
+				<GameMoveInfo currentMove={currentMove} nextMove={nextMove} />
+			}
+			gameCells={cells.map((cell, i) => (
+				<GameCell
+					disabled={!!winnerSymbol}
+					isWinner={winnerSequence?.includes(i)}
+					key={i}
+					onClick={() => handleCellClick(i)}
+					symbol={cell}
 				/>
 			))}
 		/>
