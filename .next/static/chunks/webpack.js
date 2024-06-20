2fec8b8b46f671a8e7b2da004d10bff0604598c4
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2024-06-20 11:36:21 +0300

    refactor: we made a layout for the playing field

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "316996c60cfa5103"; }
+/******/ 		__webpack_require__.h = function() { return "4f5e5f95da860b94"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
